667bea0dbf5e0f6950ab78de5ba279f0b3c24ca6
ArPal <aritrarpal@gmail.com>
2023-01-02 22:53:28 +0530

    Println functionality

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import "fmt" // format package
 // REF: https://www.youtube.com/watch?v=yyUHQIec83I
 
 func main() {
-	fmt.Print("Hello world")
-	fmt.Println("Hello again")
+	var conferenceName = "Go Conference"
+	const confernceTickets = 50
+	var remainingTickets = confernceTickets
+
+	fmt.Printf("Welcome to %v our booking application\n", conferenceName)
+	fmt.Println("We have a total of", confernceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here")
+
 }

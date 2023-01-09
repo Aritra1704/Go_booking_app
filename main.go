08fa73a79e734cdadaeaefd5476385879160133c
ArPal <aritrarpal@gmail.com>
2023-01-09 21:48:59 +0530

    intro to array

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import "fmt" // format package
 func main() {
 	var conferenceName string = "Go Conference"
 	const confernceTickets int = 50
-	var remainingTickets int = confernceTickets
+	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("Welcome to %v our booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confernceTickets, remainingTickets)
@@ -16,7 +17,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name:")
 	fmt.Scan(&firstName)
@@ -30,6 +31,15 @@ func main() {
 	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array %v\n", bookings)
+	fmt.Printf("The first value %v\n", bookings[0])
+	fmt.Printf("The array type %T\n", bookings)
+	fmt.Printf("The array length %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",
 		firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaning for %v\n", remainingTickets, conferenceName)
 }

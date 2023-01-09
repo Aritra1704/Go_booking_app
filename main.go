fdbafdd091688bb219f6831bc774babdbdd59442
ArPal <aritrarpal@gmail.com>
2023-01-09 21:27:00 +0530

    scanning input parameters

--- main.go
+++ main.go
@@ -3,20 +3,33 @@ package main
 import "fmt" // format package
 
 // REF: https://www.youtube.com/watch?v=yyUHQIec83I
-
+// 53:59
 func main() {
-	var conferenceName = "Go Conference"
-	const confernceTickets = 50
-	var remainingTickets = confernceTickets
+	var conferenceName string = "Go Conference"
+	const confernceTickets int = 50
+	var remainingTickets int = confernceTickets
 
 	fmt.Printf("Welcome to %v our booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confernceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Aritra"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets", userName, userTickets)
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",
+		firstName, lastName, userTickets, email)
 }
